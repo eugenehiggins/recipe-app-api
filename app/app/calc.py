334b45cf7bdd25f788ebe5bcdcc68f4bf3e66b2b
Gene Higgins <genotype@gknow.com>
2022-06-06 08:47:03 -0600

    created core app

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 Calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     """Add x and y return result"""
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """subtract x and y"""
     return y - x
---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,7 @@
+"""
+Django command to wait for the database to be available
+"""
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    
