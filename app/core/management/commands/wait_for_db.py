14787bfa8d2e77422077ee1d347485622bd721a6
Gene Higgins <genotype@gknow.com>
2022-06-08 08:30:40 -0600

    wrote test and model for admin customization

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,12 +1,11 @@
 """
 Django command to wait for the database to be available
 """
-import time 
+import time
 
 from django.core.management.base import BaseCommand
-
-from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
+from psycopg2 import OperationalError as Psycopg2Error
 
 
 class Command(BaseCommand):
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,9 +2,8 @@
 tests for models
 """
 
-from django.test import TestCase
-
 from django.contrib.auth import get_user_model
+from django.test import TestCase
 
 
 class ModelTests(TestCase):
