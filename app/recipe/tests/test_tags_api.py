798184e0a866603a40ce5d156af14c36baf7c40b
Gene Higgins <genotype@gknow.com>
2022-06-14 13:11:08 -0600

    added ingredients feature; updated Docker file for image api

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -47,6 +47,7 @@ class PrivateTagsApiTests(TestCase):
         self.client.force_authenticate(self.user)
 
     def test_retrieve_tags(self):
+        """Test retrieving a list of tags."""
         Tag.objects.create(user=self.user, name='Vegan')
         Tag.objects.create(user=self.user, name='Dessert')
 
